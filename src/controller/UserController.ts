73fd2a90f9f44d59e4999719eac58c4648370e0d
Hugo Oliveira reis Passos Dos Santos <pet0159851@aluno.firjansenaisesi.com.br>
2026-04-15 21:25:01 -0300

    estudo2

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,5 +1,6 @@
 import { AppDataSource } from "../data-source";
 import { User } from "../entity/User";
+import type { Request, Response } from "express";
 
 export class UserController{
     private userRepository = AppDataSource.getRepository(User);
@@ -7,9 +8,69 @@ export class UserController{
     async create(req: Request, res: Response){
         try{
             const {firstname, lastname} = req.body;
-            const newUser = this.userRepository.create({firstname, lastname})
+            const newUser = this.userRepository.create({firstname, lastname});
             await this.userRepository.save(newUser)
-            return res.status(201).json({ error: error.message});
-        }catch
+            return res.status(201).json({newUser});
+        }catch( error: unknown){
+            if(error instanceof Error){
+                return res.status(400).json({error: error.message});
+            }
+            return res.status(500).json({ error: "Ocorreu um erro inesperado ao criar o usuário."});
+          
+        }
     }
+
+    async update(req:Request, res:Response){
+       try{ const id = Number(req.params.id)
+        const {firstname, lastname} = req.body
+        if(isNaN(id)){
+            res.status(400).json({message: "id inválido!"})
+        }
+        const user = await this.userRepository.findOneBy({id});
+        if(!user){
+            res.status(404).json({ message: "Usuário não encontrado!"});
+        }
+        user.firstname = firstname?? user.firstname;
+        user.lastname = lastname?? user.lastname;
+        await this.userRepository.save(user);
+        return res.json(user);
+    }catch (error: unknown){
+        if( error instanceof Error){
+            return res.status(400).json({ error: error.message})
+        }
+        return res.status(500).json({message: "Um erro desconhecido ocorreu!"})
+    }
+    }
+
+
+    async list(req: Request, res: Response){
+        try {
+            const users = await this.userRepository.find();
+            return res.json(users);
+        } catch (error: unknown){
+            if (error instanceof Error){
+                return res.status(400).json({ error: error.message});
+            }
+            return res.status(500).json({ error: "Ocorreu um erro inesperado ao criar o usuário."});
+        }
+    }
+    async delete(req: Request, res: Response ){
+        try{
+            const id = Number(req.params.id)
+            if(isNaN(id)){
+                res.status(400).json({message: "id inválido!"})
+            }
+            const result = await this.userRepository.delete(id)
+            if(result.affected === 0){
+                res.status(404).json({message: "Usuário não encontrado"})
+            }
+            return res.status(204).send()
+        }catch (error: unknown){
+            if( error instanceof Error){
+                return res.status(400).json({ error: error.message})
+            }
+            return res.status(500).json({message: "Um erro desconhecido ocorreu!"})
+        }
+    }
+   
 }
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,16 @@
 import "reflect-metadata";
+import express, { Application } from "express"
 import { AppDataSource } from "./data-source";
-import { User } from "./entity/User";
+import { userRoutes } from "./routes/userRoutes";
 
-async function main(){
-try{
-    await AppDataSource.initialize();
-    console.log("Banco conectado!")
-    const userRepository = AppDataSource.getRepository(User);
+const app: Application = express();
+app.use(express.json());
+app.use("/api/users", userRoutes)
 
-    const newUser = userRepository.create({
-        firstname: "Marco",
-        lastname: "Hansen",
-    });
-    await userRepository.save(newUser);
-    const allUsers = await userRepository.find();
-    console.log("Usuarios cadastrados: ", allUsers);
-
-}catch (error){
-    console.log("Error: ", error)
-}
-
-}
-
-main();
+AppDataSource.initialize().then(
+    ()=> {
+        console.log("Banco conectado!");
+        app.listen(process.env.PORT, ()=> {
+            console.log(`Servidor rodando em http://localhost:${process.env.PORT}`);
+        });
+    }).catch((error) => console.log("Erro ao conectar no banco: ", error));
